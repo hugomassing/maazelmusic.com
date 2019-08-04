e54e70784c96dce13b7e210b5985c3e2dc540e8d
Hugo Massing <hugomassing@users.noreply.github.com>
2019-08-04 09:58:09 +0200

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const config = {
     headerSize: '70px',
     footerSize: '70px',
     artistName: 'Maazel',
-    tagline: 'Crating Music',
+    tagline: 'Creating Music',
     actionButtonLabel: 'Listen Now',
     actionButtonUrl: 'https://soundcloud.com/maazelbeats',
     backgroundUrl: 'static/background.jpg',
