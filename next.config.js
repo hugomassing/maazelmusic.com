57c0f9ddd21425789bf0c461491f11b1a0f64f44
Hugo Massing <hugo.massing@easy.movie>
2019-09-16 15:42:56 +0200

    changed config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
 const nextConfig = {
-  assetPrefix: '/',
   webpack: config => {
     config.module.rules.push({
       test: /\.md$/,
