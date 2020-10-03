2646202d4a8f2dbb5c68f5be87000644861513c0
Hugo Massing <hugo.massing@gmail.com>
2020-10-04 01:06:34 +0200

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -31,7 +31,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-179566239-1"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'UA-179566239-1');`,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
