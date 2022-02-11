06d8ee79c4c12f179d238cd94fc680749e2b617a
thomashesry <hptitpunk@gmail.com>
2022-02-11 08:55:27 +0100

    Addint 'The Truth'

--- constants/releases.js
+++ constants/releases.js
@@ -1,5 +1,15 @@
 const releases = [
   {
+    title: "The Truth",
+    artist: "Maazel & Sam Lamar",
+    year: "2021",
+    label: "Monstercat",
+    streamUrl: "https://monster.cat/thetruth",
+    artworkUrl:
+      "https://i1.sndcdn.com/artworks-Ov486H57Hy9dXGRr-RlE6zw-t500x500.jpg",
+    audio: "https://soundcloud.com/monstercat/maazel-sam-lamar-the-truth",
+  },
+  {
     title: "Crashing Down",
     artist: "Maazel",
     year: "2021",
